bb782eb18da3a735b728c5d1f52cfd30eb7605ea
Meekow <m.y.supriatna@gmail.com>
2022-02-13 09:58:21 +0100

    Update prometheus_uninstall.sh

--- prometheus_uninstall.sh
+++ prometheus_uninstall.sh
@@ -8,10 +8,17 @@ sudo deluser prometheus
 sudo rm -rf /etc/prometheus
 sudo rm -rf /var/lib/prometheus
 sudo rm -rf /tmp/prometheus
+sudo rm -rf /usr/local/go
+sudo rm -rf 
 
 sudo rm /usr/local/bin/prometheus
 sudo rm /usr/local/bin/promtool
 
 sudo rm /etc/systemd/system/prometheus.service
+
+sudo apt purge nginx
+sudo apt purge build-essential
+sudo apt purge libonig-dev
+
 
 sudo systemctl daemon-reload
